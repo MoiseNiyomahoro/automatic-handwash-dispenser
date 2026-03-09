342f7001458cecc65cc148c11f79f95a17a384d2
Moise NIYOMAHORO <niyomahoromoise@gmail.com>
2026-03-09 20:34:03 +0200

    added screen

--- Automatic-handwash-dispenser.ino
+++ Automatic-handwash-dispenser.ino
@@ -1,43 +1,49 @@
+#include<LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
+
 int trigPin = 9;
-int  echoPin = 10;
+int echoPin = 10;
 int relayPin = 7;
-int led=5;
+int led = 5;
 long duration;
 int distance;
 
 void setup() {
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
-pinMode(relayPin, OUTPUT);
-pinMode(ledpin, OUTPUT);
-Serial.begin(9600);
+  lcd.init();
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(relayPin, OUTPUT);
+  pinMode(ledpin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-duration = pulseIn(echoPin, HIGH);
-distance = duration * 0.034 / 2;
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  lcd.setCursor(0,0);
+  lcd.print("DISTANCE: ");
+  lcd.println(distance);
 
-Serial.print("Distance: ");
-Serial.println(distance);
+  Serial.print("Distance: ");
+  Serial.println(distance);
 
-if(distance <= 10)
-{
-digitalWrite(relayPin, HIGH);
-delay(2000);
-digitalwrite(ledpin,HIGH);
-}
-else
-{
-digitalWrite(relayPin, LOW);
-digitalWrite(ledpin, LOW);
-}
+  if (distance <= 10) {
+    digitalWrite(relayPin, HIGH);
+    delay(2000);
+    lcd.setCursor(1,0);
+    lcd.print("WASH YOUR HANDS");
+    digitalwrite(ledpin, HIGH);
+  } else {
+    digitalWrite(relayPin, LOW);
+    digitalWrite(ledpin, LOW);
+  }
 
-delay(500);
+  delay(500);
 }
